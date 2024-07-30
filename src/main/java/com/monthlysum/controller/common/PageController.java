658fa5bf381ddcea7830bf7dd7c44f3bcc926cee
liujianping <136174298@qq.com>
2024-07-30 13:25:23 +0900

    ico error

--- src/main/java/com/monthlysum/controller/common/PageController.java
+++ src/main/java/com/monthlysum/controller/common/PageController.java
@@ -2,8 +2,10 @@ package com.monthlysum.controller.common;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class PageController {
@@ -11,5 +13,11 @@ public class PageController {
     public String showPage(@PathVariable("page") String page, Model model) {
         model.addAttribute("pageName", page);
         return page; // 返回与路径变量同名的模板
+    }
+
+    @GetMapping("favicon.ico")
+    @ResponseBody
+    void returnNoFavicon() {
+        // 返回空响应
     }
 }
